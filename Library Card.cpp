efce19723044c09edab7bbaa1a1543424f47c87b
WS239618 <55600269+WS239618@users.noreply.github.com>
2020-10-07 14:54:06 +0100

    I have implemented a few getters to retrieve the private attributes, and a function that allows for one to take out a book and subsequently decrement it from the number of copies attribute.

--- Library Card.cpp
+++ Library Card.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 class Book {
 private:
 	std::string m_sBookTitle;
@@ -22,6 +23,20 @@ public:
 	int GetNumberOfCopies() {
 		return m_iNumberOfCopies;
 	}
+	bool DecrementNumberOfCopies()
+	{
+		if (m_iNumberOfCopies > 0)
+		{
+			m_iNumberOfCopies--;
+		}
+		else {
+			std::cout << "Sorry, there are no more books left to borrow for " << m_sBookTitle << std::endl;
+		}
+	}
+	bool IncrementNumberOfCopies()
+	{
+		m_iNumberOfCopies++;
+	}
 };
 class Library {
 private:
@@ -31,13 +46,24 @@ public:
 		m_objBooks.push_back({sBookTitle, sAuthor, iNumberOfCopies});
 	}
 	void DisplayBooks() {
-		int iCount = 0;
+		int iCount = 1;
 		for (Book& objBook : m_objBooks) {
 			std::cout << "--------" << std::endl;
-			std::cout << "Book " << iCount + 1 << objBook.GetTitle() << std::endl;
-			std::cout << objBook.GetAuthor() << std::endl;
-			std::cout << objBook.GetNumberOfCopies() << std::endl;
+			std::cout << "Book " << iCount++ << std::endl;
+			std::cout << "Title: "<< objBook.GetTitle() << std::endl;
+			std::cout << "Author: "<< objBook.GetAuthor() << std::endl;
+			std::cout << "Number of copies available: " << objBook.GetNumberOfCopies() << std::endl;
 	
+		}
+		std::cout << "-------" << std::endl;
+	}
+	void TakeOutBook(std::string sTitle) {
+		for (Book& objBook : m_objBooks)
+		{
+			if (sTitle == objBook.GetTitle())
+			{
+				objBook.DecrementNumberOfCopies();
+			}
 		}
 	}
 };
@@ -49,11 +75,18 @@ void AddBooks(Library &objLibrary)
 	objLibrary.AddBookToLibrary("Harry Potter - Prisoner of Askaban", "J.K Rowling", 12);
 	objLibrary.AddBookToLibrary("Harry Potter - Deathly Hallows", "J.K Rowling", 9);
 }
+void TakeOutBook(Library &objLibrary) {
+	std::string sName = "";
+	std::cout << "Enter the book you wish to take out: ";
+	std::getline(std::cin >> std::ws, sName);
+	objLibrary.TakeOutBook(sName);
+}
 
 int main()
 {
 	Library objLibrary;
 	AddBooks(objLibrary);
 	objLibrary.DisplayBooks();
+	TakeOutBook(objLibrary);
 }
 
